4740fb7139074ba4db88514c1b0cfa5e0a5ec1e9
ManuelMolino <mmolmil158@g.educaand.es>
2026-02-11 12:47:02 +0100

    Ejercicio composicion con Map como colección y joptionpane (finalización)

--- src/main/java/ejercicios/MainVentanas.java
+++ src/main/java/ejercicios/MainVentanas.java
@@ -1,6 +1,8 @@
 package ejercicios;
 
 import javax.swing.*;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 public class MainVentanas {
@@ -32,7 +34,7 @@ public class MainVentanas {
                     eliminarOrdenador();
                     break;
                 case 2:
-                    System.out.println("llamamos a un método que actualice");
+                    actualizarOdenador();
                     break;
                 case 3:
                     listarOrdenadores();
@@ -45,6 +47,21 @@ public class MainVentanas {
             }
         }
 
+    }
+
+    private static void actualizarOdenador() {
+        Object seleccion = getSeleccion();
+        String id = (String) seleccion;
+        String sRAM = getDialog("Introduce la RAM","nuevo ordenador");
+        int ram = Integer.parseInt(sRAM);
+        String sVelocidad = getDialog("Introduce la velocidad","nuevo ordenador");
+        double velocidad = Double.parseDouble(sVelocidad);
+        String sDisco = getDialog("Introduce capacidad disco","nuevo ordenador");
+        int disco = Integer.parseInt(sDisco);
+        int iGrafica = getGrafica();
+        boolean bGrafica = iGrafica != 0;
+        Ordenador ordenador = new Ordenador(id, ram, velocidad, bGrafica, disco);
+        aulaInformatica.actualizarOrdenador(ordenador);
     }
 
     private static void listarOrdenadores() {
@@ -64,7 +81,6 @@ public class MainVentanas {
 
     private static void insertarOrdenador() {
         //id ID0, ID1, ID2, ID3..
-
         String id = UUID.randomUUID().toString();
         String sRAM = getDialog("Introduce la RAM","nuevo ordenador");
         int ram = Integer.parseInt(sRAM);
@@ -72,7 +88,14 @@ public class MainVentanas {
         double velocidad = Double.parseDouble(sVelocidad);
         String sDisco = getDialog("Introduce capacidad disco","nuevo ordenador");
         int disco = Integer.parseInt(sDisco);
-        int iGrafica = JOptionPane.showOptionDialog(
+        int iGrafica = getGrafica();
+        boolean bGrafica = iGrafica != 0;
+        Ordenador ordenador = new Ordenador(id, ram, velocidad, bGrafica, disco);
+        aulaInformatica.addOrdenador(ordenador);
+    }
+
+    private static int getGrafica() {
+        return JOptionPane.showOptionDialog(
                 null,
                 "Gráfica integrada",
                 "nuevo ordenador",
@@ -81,15 +104,32 @@ public class MainVentanas {
                 null,
                 null,
                 null
-                );
-        System.out.println(iGrafica);
-        boolean bGrafica = iGrafica != 0;
-        Ordenador ordenador = new Ordenador(id, ram, velocidad, bGrafica, disco);
-        aulaInformatica.addOrdenador(ordenador);
+        );
     }
 
     private static void eliminarOrdenador() {
+        Object seleccion = getSeleccion();
+        aulaInformatica.eliminarOrdenador((String) seleccion);
+    }
 
-        aulaInformatica.eliminarOrdenador("idiid");
+    private static Object getSeleccion() {
+        return JOptionPane.showInputDialog(
+                null,
+                "Seleccione opcion",
+                "Selector de opciones",
+                JOptionPane.QUESTION_MESSAGE,
+                null,  // null para icono defecto
+                getClaves(),
+                getClaves()[0]);
+    }
+
+    private static Object[] getClaves() {
+        //obtener el Map: llamo al getter
+        Map<String, Ordenador> ordenadoresMap = aulaInformatica.getOrdenadores();
+        //obtener el Set de claves: KeySet
+        Set<String > idsOrdenadores = ordenadoresMap.keySet();
+        //crear un array de Object del tamaño del Set
+        Object[] claves = idsOrdenadores.toArray();
+        return claves;
     }
 }
